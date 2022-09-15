c5bbe782f32764927e1100cc443fcb275e9cd3c8
yeewong3 <50363180+yeewong3@users.noreply.github.com>
2022-09-15 15:28:41 +0800

    Update the algorithms

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Covid19_worldmap_newcase
-World map for covid19 new cases on 7 Sep 2022
+World map for daily covid19 new cases.
 Data Source from: https://github.com/owid/covid-19-data/blob/0bc7d32215745ef5122cb11ff6adb30a63907b34/public/data/latest/owid-covid-latest.json
--- covid19_worldmap.py
+++ covid19_worldmap.py
@@ -8,7 +8,20 @@ url = 'https://github.com/owid/covid-19-data/blob/master/public/data/latest/owid
 covid19 = pd.read_html(url)
 
 file = json.loads(covid19[0][1].values.tolist()[0])
+data = pd.DataFrame(file).transpose()[['location','new_cases', 'last_updated_date']].reset_index()
+codebook = pd.Series(COUNTRIES, name = 'location').reset_index()
+data_coded = data.merge(codebook, how = 'left', on='location', suffixes =('_data','_map'))
 
+value = dict(zip(data_coded.index_map, data_coded.new_cases))
+
+# Create map
+title = "Daily Cases for covid 19 over the world, updated on "+ str(max(data_coded.last_updated_date)[:10])
+worldmap = pygal.maps.world.World()
+worldmap.title = title
+worldmap.add( "New cases", value)
+worldmap.render_to_file('covid19.svg')
+
+'''
 data = {}
 date = []
 
@@ -25,8 +38,5 @@ for country, new_cases in data.items():
             if countrycode != None:
                 value[countrycode] = new_cases
 
-title = "Daily Cases for covid 19 over the world, updated on "+ str(max(date)[:10])
-worldmap = pygal.maps.world.World()
-worldmap.title = title
-worldmap.add( "New cases", value)
-worldmap.render_to_file('covid19.svg')
+
+'''
